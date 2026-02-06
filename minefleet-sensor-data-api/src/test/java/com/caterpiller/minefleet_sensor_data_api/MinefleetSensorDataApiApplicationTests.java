268aced65bb4d65bf6c0b92702b352b36beb21bc
Sama <sama.alkamachy99@gmail.com>
2026-02-06 20:56:19 +1000

    completes planning and design stage of application

---
+++ minefleet-sensor-data-api/src/test/java/com/caterpiller/minefleet_sensor_data_api/MinefleetSensorDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caterpiller.minefleet_sensor_data_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinefleetSensorDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
